7cc21957fa39e4dbee953248489f5b31b63aa146
Kadin <108703716+KadinRelefourd@users.noreply.github.com>
2025-04-28 06:36:16 -0400

    Create embedding.py

---
+++ src/embedding.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import yfinance as yf
+
+def download_data(tickers, start_date, end_date, interval='1d'):
+    """
+    Download adjusted close prices for each ticker and return a DataFrame.
+    """
+    df_list = []
+    for t in tickers:
+        df_t = (
+            yf.download(
+                t,
+                start=start_date,
+                end=end_date,
+                interval=interval,
+                progress=False
+            )
+            .get('Adj Close')
+            .rename(t)
+        )
+        df_list.append(df_t)
+    price_df = pd.concat(df_list, axis=1)
+    price_df.index.name = 'Date'
+    return price_df
